9b91eb9bf8f6b4a5a2ad4f6b8e026ceb192dc681
anestv <anestv@users.noreply.github.com>
2014-10-28 18:09:00 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,51 @@
+
+import json, os
+import external.requests as requests
+
+SERVER_ADDR = 'http://localhost'
+PAGES_PATH = '/pa/'
+OUTPUT_PATH = 'output/'
+TEST_CASES_FILE = 'testCases.json'
+
+
+if not os.path.exists(OUTPUT_PATH):
+  os.mkdir(OUTPUT_PATH)
+
+jsonString = open(TEST_CASES_FILE, 'r').read()
+
+allPages = json.loads(jsonString)
+
+
+def sendGet(url, params, fout):
+  r = requests.get(url, params = params)
+  
+  print('GET '+url, r.status_code, sep = '\n')
+  print('GET : ' + url, r.status_code, r.headers, r.text, sep = '\n  ',
+        end = '============================\n', file = fout)
+
+
+def sendPost(url, data, fout):
+  if 'GETquery' in data:
+    params = data['GETquery']
+    del data['GETquery']
+  else:
+    params = {}
+  
+  r = requests.post(url, params = params, data = data)
+  
+  print('POST '+url, r.status_code, sep = '\n')
+  print('POST : ' + url, r.status_code, r.headers, r.text, sep = '\n  ',
+        end = '============================\n', file = fout)
+
+
+for page in allPages:
+  path = SERVER_ADDR + PAGES_PATH + page['file']
+  output = open(OUTPUT_PATH + page['file'] + '.txt', 'w')
+  
+  for get in page['get']:
+    sendGet(path, get, output)
+  
+  for post in page['post']:
+    sendPost(path, post, output)
+  
+  output.close()
