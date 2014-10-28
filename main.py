df36d9266d9b9702a0e238d5d83c6f6ba3bfa4e2
anestv <anestv@users.noreply.github.com>
2014-10-28 19:38:03 +0200

    Added settings file

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 
-import json, os
+import json, os, configparser
 import external.requests as requests
 
-SERVER_ADDR = 'http://localhost'
-PAGES_PATH = '/pa/'
-OUTPUT_PATH = 'output/'
-TEST_CASES_FILE = 'testCases.json'
-USE_SESSION = False
+try:
+  config = configparser.ConfigParser()
+  config.read('settings.ini')
+  s = config['Configuration']
+except:
+  s = {}
+
+SERVER_ADDR = s.get('SERVER_ADDR', 'http://localhost')
+PAGES_PATH = s.get('PAGES_PATH', '/')
+OUTPUT_PATH = s.get('OUTPUT_PATH', 'output/')
+TEST_CASES_FILE = s.get('TEST_CASES_FILE', 'testCases.json')
+USE_SESSION = s.get('USE_SESSION', False) == 'True'
+# == is to convert string 'True' to bool. We
+# do not getboolean because s may be a dict
 
 
 if not os.path.exists(OUTPUT_PATH):
