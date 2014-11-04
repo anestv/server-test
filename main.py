0f68c19f9029671777d03ab78266c7a8cab279d8
anestv <anestv@users.noreply.github.com>
2014-11-04 18:42:06 +0200

    OOP. Exits when an expectation is not met
    JSON objects represent individual requests, not pages
    They have a property "expect" which defines HTTP status code and header
    expectations

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 
 import json, os, configparser
 from re import sub
-import external.requests as requests
+from myRequest import request
+
+print('# Starting server-test...')
 
 try:
   config = configparser.ConfigParser()
@@ -12,58 +14,23 @@ except:
 
 SERVER_ADDR = s.get('SERVER_ADDR', 'http://localhost')
 PAGES_PATH = s.get('PAGES_PATH', '/')
-OUTPUT_PATH = s.get('OUTPUT_PATH', 'output/')
 TEST_CASES_FILE = s.get('TEST_CASES_FILE', 'testCases.json')
 USE_SESSION = s.get('USE_SESSION', False) == 'True'
 # == is to convert string 'True' to bool. We
 # do not getboolean because s may be a dict
 
-
-if not os.path.exists(OUTPUT_PATH):
-  os.mkdir(OUTPUT_PATH)
-
-if USE_SESSION:
-  requests = requests.Session()
-
 jsonString = open(TEST_CASES_FILE, 'r').read()
 
-allPages = json.loads(jsonString)
+allRequests = json.loads(jsonString)
 
+for req in allRequests:
+  url = SERVER_ADDR + PAGES_PATH + req['page']
+  
+  reqObj = request(url, req.get('expect', {}), req.get('get', {}), req.get('post', {}))
+  
+  if USE_SESSION and 'session' in req:
+    reqObj.session = int(req['session'])
+  
+  reqObj.send(USE_SESSION)
 
-def sendGet(url, params, fout):
-  r = requests.get(url, params = params)
-  
-  print('GET '+url, r.status_code, sep = '\n')
-  print('GET : ' + url, r.status_code, r.headers, r.text, sep = '\n  ',
-        end = '============================\n', file = fout)
-
-
-def sendPost(url, data, fout):
-  if 'GETquery' in data:
-    params = data['GETquery']
-    del data['GETquery']
-  else:
-    params = {}
-  
-  r = requests.post(url, params = params, data = data)
-  
-  print('POST '+url, r.status_code, sep = '\n')
-  print('POST : ' + url, r.status_code, r.headers, r.text, sep = '\n  ',
-        end = '============================\n', file = fout)
-
-
-for page in allPages:
-  url = SERVER_ADDR + PAGES_PATH + page['file']
-  
-  filename = sub(r'[\\\|\*"?:/<>%]', '!', page['file'])
-  output = open(OUTPUT_PATH + filename + '.txt', 'w')
-  
-  if 'get' in page:
-    for get in page['get']:
-      sendGet(url, get, output)
-  
-  if 'post' in page:
-    for post in page['post']:
-      sendPost(url, post, output)
-  
-  output.close()
+print('# All tests finished successfully! server-test closing...')
